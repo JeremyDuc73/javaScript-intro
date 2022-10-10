ceb4389cca28b294856a053ec07d6d2a27e19d09
jduc <jduc@gaming.tech>
2022-10-10 16:56:33 +0200

    soir 10/10

--- main.js
+++ main.js
@@ -6,18 +6,44 @@ let finalTest = test.join('')
 console.log(finalTest)
 console.log(word[2])
 
-const monH1 = document.querySelector(".rouge")
 
+
+
+const monH1 = document.querySelector(".rouge")
 setInterval(()=>{monH1.classList.toggle("bleu")}, 500);
+
+
+
 
 const monP = document.querySelector(".monP")
 let mots = ["Bonjour" , "Hola" , "Hello" , "こんにちは"]
-setInterval(
-    changeText, 5000
-)
-function changeText(){
-    for (let x in mots) {
-        console.log(mots[x])
-        monP.innerHTML = mots[x]
-    }
+for (let i = 0; i<mots.length ; i++){
+    setTimeout(()=>{monP.innerHTML = mots[i]},1000*i)
 }
+
+
+
+
+let motInterdit = "dindon"
+let unePhrase = "le doux dos dodu du dindon farci se dandinait doucement"
+let unTableau = unePhrase.split(" ")
+unTableau.forEach(mot=>{
+    let paragraphe = document.createElement('p');
+    let index = unTableau.indexOf(mot)
+    if (index % 2 == 0){
+        paragraphe.classList.add("cyan")
+    }
+    else{
+        paragraphe.classList.add("rose")
+    }
+
+
+    if(mot == motInterdit){
+        paragraphe.innerHTML = "poulet"
+    }
+    else {
+            paragraphe.innerHTML = mot
+    }
+    let body = document.querySelector("body");
+    body.appendChild(paragraphe);
+})
