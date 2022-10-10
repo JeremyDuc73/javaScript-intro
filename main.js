d3925e793157996700460a64d9d4b668e9c4956b
jduc <jduc@gaming.tech>
2022-10-10 13:01:21 +0200

    midi 10/10

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("coucou")
+let word = "bonjour"
+let test = word.split("")
+console.log(test)
+test.splice(4, 1)
+let finalTest = test.join('')
+console.log(finalTest)
+console.log(word[2])
+
+const monH1 = document.querySelector(".rouge")
+
+setInterval(()=>{monH1.classList.toggle("bleu")}, 500);
+
+const monP = document.querySelector(".monP")
+let mots = ["Bonjour" , "Hola" , "Hello" , "こんにちは"]
+setInterval(
+    changeText, 5000
+)
+function changeText(){
+    for (let x in mots) {
+        console.log(mots[x])
+        monP.innerHTML = mots[x]
+    }
+}
